c762b79c6e7e03ce78a214ee4a427a5098721272
ml027 <108434538+ml027@users.noreply.github.com>
2022-07-20 15:56:58 -0400

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import "./styles.css"
 export default function Navbar() {
   return (
     <nav className="nav">
-      <a href ="/uoGamingFinal" className="site-title">
+      <a href ="https://ml027.github.io/uoGamingFinal/" className="site-title">
         uoGaming
       </a>
       
